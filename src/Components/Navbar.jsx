f63c32763d363fe638cb23796d0a9bbe297218c9
maruthishalivahana <maruthikummari553@gmail.com>
2025-09-27 15:52:48 +0530

    implimented redux store in our application

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
                 </div>
                 <div className="flex gap-2">
                     <button className="btn btn-neutral bg-base-300" onClick={() => navigate('/login')}>Login</button>
-                    {user && <div className="dropdown dropdown-end mx-8">
+                    {user._id && < div className="dropdown dropdown-end mx-8">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
                                 <img
@@ -39,7 +39,7 @@ function Navbar() {
                         </ul>
                     </div>}
                 </div>
-            </div>
+            </div >
         </>
     )
 }
