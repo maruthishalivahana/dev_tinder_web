d665c3d0191dfa0f55e0c6d0c2f7926ab59958c4
maruthishalivahana <maruthikummari553@gmail.com>
2025-09-27 12:18:44 +0530

    setup routing to application

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import Navbar from "./Components/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./Components/Login"
+import Home from "./Components/Home"
 function App() {
   return (
     <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
       <Navbar />
+
     </>
   )
 }
