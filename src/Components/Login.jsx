b7f6bb6587fcac5136bdd7eb21bd54dcb95352bb
maruthishalivahana <maruthikummari553@gmail.com>
2025-10-19 19:20:45 +0530

    handled tokens

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import api from '../utils/api'
 import { toast, ToastContainer } from "react-toastify";
 import { addUser } from '../utils/userSlice'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { BASE_URL } from '../utils/constants';
-function Login() {
 
+function Login() {
     const [emailId, setEmailId] = useState("")
     const [password, setPassword] = useState("")
     const [firstName, setFirstName] = useState("")
@@ -17,56 +16,58 @@ function Login() {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const handleLogin = async () => {
 
+    const handleLogin = async () => {
         try {
-            const res = await api.post('/login', {
+            const res = await api.post(`${BASE_URL}/login`, {
                 email: emailId,
                 password
             }, { withCredentials: true })
 
-            navigate('/')
+
+            if (res.data.token) {
+                localStorage.setItem("token", res.data.token)
+            }
+
             dispatch(addUser(res.data.user))
-            const firstName = res.data.user.firstName
-            toast.success(`welcome back! ${firstName}`)
-        } catch (error) {
-            setError(error.response.data.message)
-            toast.error(error)
+            navigate('/')
+            toast.success(`Welcome back! ${res.data.user.firstName}`)
+        } catch (err) {
+            const msg = err.response?.data?.message || err.message
+            setError(msg)
+            toast.error(msg)
         }
     }
-    const handleSignUp = async () => {
 
+
+    const handleSignUp = async () => {
         try {
-            const res = await api.post('/register', {
+            const res = await api.post(`${BASE_URL}/register`, {
                 firstName,
                 lastName,
                 email: emailId,
                 password
-            })
+            }, { withCredentials: true })
 
-            navigate('/editprofile')
+
+            if (res.data.token) {
+                localStorage.setItem("token", res.data.token)
+            }
+
             dispatch(addUser(res.data.user))
-            // const firstName = res.data.user.firstName
-            // toast.success(`welcome ! ${firstName}`)
-        } catch (error) {
-            setError(error.message)
-            toast.error(error)
+            navigate('/editprofile')
+        } catch (err) {
+            const msg = err.response?.data?.message || err.message
+            setError(msg)
+            toast.error(msg)
         }
     }
 
-
-    const handleToggle = () => {
-        if (!isLogin) {
-            setLogin(true)
-        } else {
-            setLogin(false)
-        }
-
-    }
-
+    const handleToggle = () => setLogin(!isLogin)
 
     return (
         <div className="hero bg-base-200 min-h-screen">
+            <ToastContainer />
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">{isLogin ? "login" : "signup"} now!</h1>
@@ -79,37 +80,31 @@ function Login() {
                         <fieldset className="fieldset">
                             {!isLogin && <label className="label">FirstName</label>}
                             {!isLogin && <input type="text" className="input" placeholder="FirstName"
-                                value={firstName}
-                                onChange={(e) => setFirstName(e.target.value)}
-
-                            />}
+                                value={firstName} onChange={(e) => setFirstName(e.target.value)} />}
                             {!isLogin && <label className="label">LastName</label>}
                             {!isLogin && <input type="text" className="input" placeholder="LastName"
-                                value={lastName}
-                                onChange={(e) => setlastName(e.target.value)}
-
-                            />}
+                                value={lastName} onChange={(e) => setlastName(e.target.value)} />}
                             <label className="label">Email</label>
                             <input type="email" className="input" placeholder="Email"
-                                value={emailId}
-                                onChange={(e) => setEmailId(e.target.value)}
-
-                            />
+                                value={emailId} onChange={(e) => setEmailId(e.target.value)} />
                             <label className="label">Password</label>
                             <input type="password" className="input" placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                value={password} onChange={(e) => setPassword(e.target.value)} />
 
-                            />
-
-
-                            <div className='flex justify-around '><a className="link link-hover " >Forgot password?</a>
+                            <div className='flex justify-around '>
+                                <a className="link link-hover" >Forgot password?</a>
                                 <div>
-                                    <p >{isLogin ? "if don't have account" : "if you have a account"} <span className='text-blue-400  hover:underline cursor-pointer' onClick={handleToggle}>{isLogin ? "signUp" : "Login"}</span> </p>
+                                    <p>{isLogin ? "if don't have account" : "if you have a account"}
+                                        <span className='text-blue-400 hover:underline cursor-pointer' onClick={handleToggle}>
+                                            {isLogin ? "signUp" : "Login"}
+                                        </span>
+                                    </p>
                                 </div>
                             </div>
                             <p className='text-center text-red-400 text-[14px]'>{error}</p>
-                            <button className="btn btn-neutral mt-4" onClick={isLogin ? handleLogin : handleSignUp}>{isLogin ? "Login" : "Signup"}</button>
+                            <button className="btn btn-neutral mt-4" onClick={isLogin ? handleLogin : handleSignUp}>
+                                {isLogin ? "Login" : "Signup"}
+                            </button>
                         </fieldset>
                     </div>
                 </div>
